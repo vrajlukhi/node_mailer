b5c0aa58124a5f935946b7bdab2a5dcc4f145ca6
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2023-11-01 09:47:24 +0530

    nodemailer

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const nodemailer=require("nodemailer")
+const app=express()
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'vrajlukhi@gmail.com',
+      pass: 'ljck pevu jswp kcht'
+    }
+  })
+
+app.post("/sendmail",(req,res)=>{
+    let{email,sub,text}=req.body
+    var mailOptions = {
+        from: 'vrajlukhi@gmail.com',
+        to: email,
+        subject: sub,
+        text: text
+      };
+      transporter.sendMail(mailOptions, (error, info)=>{
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(info);
+        }
+      });
+      res.status(200).send("done")
+})
+
+app.listen(8090,()=>{
+    console.log("server 8090");
+})
